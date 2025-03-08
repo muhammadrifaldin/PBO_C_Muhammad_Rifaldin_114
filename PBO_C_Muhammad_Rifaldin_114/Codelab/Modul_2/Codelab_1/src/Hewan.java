56cb7ccb93cd8fd1f5bc32ed42255c5e73d7c0a3
muhammadrifaldin <muhammadrifaldin20@gmail.com>
2025-03-08 15:24:43 +0700

    first commit

---
+++ PBO_C_Muhammad_Rifaldin_114/Codelab/Modul_2/Codelab_1/src/Hewan.java
@@ -0,0 +1,18 @@
+public class Hewan {
+    String nama;
+    String jenis;
+    String suara;
+
+    public Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara);
+        System.out.println();
+    }
+}
---
+++ PBO_C_Muhammad_Rifaldin_114/Codelab/Modul_2/Codelab_1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Hewan hewan1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
+        Hewan hewan2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
+
+        hewan1.tampilkanInfo();
+        hewan2.tampilkanInfo();
+    }
+}
